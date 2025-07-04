75ca46ee08d74a2ac4e5b60c6a4b3022ab7111e3
radioaddp <canaljuniordias@gmail.com>
2025-07-04 14:46:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, Response, stream_with_context
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -10,8 +11,12 @@ def proxy_stream():
     headers = {
         "User-Agent": "VLC/3.0.11 LibVLC/3.0.11"
     }
-    r = requests.get(SOURCE_URL, headers=headers, stream=True)
-    return Response(stream_with_context(r.iter_content(chunk_size=1024)), content_type=r.headers['Content-Type'])
+    try:
+        r = requests.get(SOURCE_URL, headers=headers, stream=True, timeout=10)
+        return Response(stream_with_context(r.iter_content(chunk_size=1024)), content_type=r.headers.get('Content-Type', 'application/vnd.apple.mpegurl'))
+    except requests.exceptions.RequestException:
+        return "Erro ao conectar ao stream original.", 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
